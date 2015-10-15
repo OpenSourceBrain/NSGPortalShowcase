f490f1195984b88da1cf51e58729f19eb12aad83
Padraig Gleeson <p.gleeson@gmail.com>
2015-10-15 17:52:58 +0100

    Another NEURON exmaple tested on NSG

---
+++ examples/NEURON/makezips.sh
@@ -0,0 +1,9 @@
+
+mv ParallelNetwork/*.dat /tmp
+rm -rf ParallelNetwork/x86_64
+zip -x ParallelNetwork/*~ -r input ParallelNetwork
+
+
+mv PythonNeuron/*.dat /tmp
+rm -rf PythonNeuron/x86_64
+zip -x PythonNeuron/*~ -r input2 PythonNeuron
