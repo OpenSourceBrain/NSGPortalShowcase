293e93d46d0901fff9360764787bdae0d42f2b4d
Padraig Gleeson <p.gleeson@gmail.com>
2021-06-09 10:51:55 +0100

    Update medium net

---
+++ NSG-R/submit_netpyne_info_admin.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source ./read_env_variables_admin_osb
+
+url=$URL/job/$UMBRELLA_APPNAME.$USER_USERNAME
+
+echo "Accessing: $url for user $ADMIN_USERNAME on behalf of $USER_USERNAME"
+
+curl -i -u $ADMIN_USERNAME:$ADMIN_PASSWORD \
+     -H cipres-appkey:$UMBRELLA_APPID \
+     -H cipres-eu:$USER_USERNAME \
+     -H cipres-eu-email:$USER_EMAIL \
+     -H cipres-eu-institution:$USER_INSTITUTION \
+     -H cipres-eu-country:$USER_COUNTRY \
+     -F tool='PY_EXPANSE' \
+     -F input.infile_=@../examples/NetPyNE/input.zip \
+     -F metadata.clientJobId=NetPyNETestAdmin \
+     -F metadata.statusEmail=true \
+     -F vparam.number_cores_=1 \
+     -F vparam.number_nodes_=1 \
+     -F vparam.tasks_per_node_=1  \
+      $url
+     
+    
--- examples/NetPyNE/makezip.sh
+++ examples/NetPyNE/makezip.sh
@@ -1,4 +1,4 @@
 
 mv MediumNet/*.dat /tmp
-rm -rf MediumNet/x86_64
+rm -rf MediumNet/x86_64 simulator.props report.medium.txt
 zip -x MediumNet/*~ -r input MediumNet
