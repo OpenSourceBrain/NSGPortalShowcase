e5f7c40cae2f1e0bce223c14c146f163259cba98
Padraig Gleeson <p.gleeson@gmail.com>
2021-06-10 17:40:39 +0100

    Workign across 2 nodes

--- NSG-R/submit_python_job_netpyne2.sh
+++ NSG-R/submit_python_job_netpyne2.sh
@@ -7,9 +7,11 @@ echo "Accessing: $URL/job/$USERNAME for user $USERNAME"
 curl -i --user $USERNAME:$PASSWORD \
      -H cipres-appkey:$DIRECT_APPID \
       $URL/job/$USERNAME \
-     -F tool='OSBPYNEURON77' \
-     -F input.infile_=@../examples/NetPyNE2/input.zip \
-     -F metadata.clientJobId=NetPyNE2_test \
+     -F tool='NEURON_EXPANSE' \
+     -F input.infile_=@../examples/NetPyNE/input.zip \
+     -F vparam.filename_=init.py \
+     -F metadata.clientJobId=NetPyNE_multinode_test \
      -F metadata.statusEmail=true \
      -F vparam.number_cores_=8 \
-     -F vparam.number_nodes_=2
+     -F vparam.number_nodes_=2 \
+     -F vparam.tasks_per_node_=1
